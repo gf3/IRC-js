7df9c0a3ce15864112ff06844f4f05213a01ccc1
Gianni Chiappetta <gianni@runlevel6.org>
2010-07-28 17:02:56 -0400

    updated: utools dependancy.

--- vendor/utools/lib/utools.js
+++ vendor/utools/lib/utools.js
@@ -1,42 +1,6 @@
 // NO U LOL - Gianni Chiappetta <gianni@runlevel6.org>
 
 /**
- * Function#bind(context[, args...]) -> Function
- * - context (Object): Object context to bind to.
- * - args (?): Optional arguments to curry.
- * 
- * Bind a function to a given `context`. Optionally curry arguments.
- * 
- * ### Examples
- * 
- *     var new_func = my_func.bind(my_object, "no u");
-**/
-if (typeof Function.prototype.bind !== "function") {
-  Object.defineProperty(Function.prototype, "bind", {
-    value: (function(){
-      var _slice = Array.prototype.slice
-      return function(context) {
-        var fn = this
-          , args = _slice.call(arguments, 1)
-        
-        if (args.length) 
-          return function() {
-            return arguments.length
-              ? fn.apply(context, args.concat(_slice.call(arguments)))
-              : fn.apply(context, args)
-          }
-         
-        return function() {
-          return arguments.length
-            ? fn.apply(context, arguments)
-            : fn.call(context)
-        }
-      }
-    })()
-  })
-}
-
-/**
  * Object#extend(source) -> Object
  * - source (Object): Source object whose properties will be duplicated.
  * 
