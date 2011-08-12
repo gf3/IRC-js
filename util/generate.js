ee6e8c42973dfca24e3eabda7f40d82d66ab0cc9
Gianni Chiappetta <gianni@runlevel6.org>
2011-08-12 03:38:07 -0400

    fixed: Remove dependency on ColouredLog in util/generate.js, fixes #41.

--- util/generate.js
+++ util/generate.js
@@ -1,8 +1,6 @@
-var ColouredLog = require( '../node_modules/coloured-log' )
-  , PanPG = require( '../node_modules/PanPG/PanPG' )
+var PanPG = require( '../node_modules/PanPG/PanPG' )
   , path = require( 'path' )
   , fs = require( 'fs' )
-  , Log
   , PEG = fs.readFileSync( path.join( __dirname, '..', 'lib', 'irc.peg' ) ).toString()
   , options =
     { 'commonjs': true
@@ -12,23 +10,23 @@ var ColouredLog = require( '../node_modules/coloured-log' )
   , filename = path.join( __dirname, '..', 'lib', 'parser.js' )
 
 /* ------------------------------ Build Parser ------------------------------ */
-Log = new ColouredLog( ColouredLog.DEBUG )
-
 try {
   parser = PanPG.generateParser( PEG, options )
   fs.writeFile( filename, parser, function( err ) {
     if ( err ) {
-      Log.error( 'Unable to write file.' )
-      console.log( err.message )
-      console.log( err.stack )
+      console.error( '✖ Unable to write file.' )
+      console.error( err.message )
+      console.error( err.stack )
+      return 1
     }
     else
-      Log.info( 'Wrote file: "' + filename + '"' )
+      console.info( '✔ Wrote file: "' + filename + '"' )
   })
 }
 catch ( e ) {
-  Log.error( 'Parser could not be generated.' )
-  console.log( e.message )
-  console.log( e.stack )
+  console.error( '✖ Parser could not be generated.' )
+  console.error( e.message )
+  console.error( e.stack )
+  return 1
 }
 
